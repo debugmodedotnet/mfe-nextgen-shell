a113c0a5326301a1be8536796c3079beecf50b63
dkumar-mbai <dkumar@matchbookai.com>
2024-07-09 12:45:52 +0530

    v1

---
+++ src/app/app.routes.ts
@@ -0,0 +1,36 @@
+import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import { loadRemoteModule } from '@angular-architects/native-federation';
+import {NotfoundComponent} from './notfound/notfound.component'
+
+export const APP_ROUTES: Routes = [
+    {
+      path: '',
+      redirectTo: 'home',
+      pathMatch: 'full',
+    },
+  
+    // Add this route:
+    {
+      path: 'powerbiadmin',
+      loadComponent: () =>
+        loadRemoteModule('powerbiadmin', './Component').then((m) => m.AppComponent),
+    },
+    {
+        path: 'price',
+        loadComponent: () =>
+          loadRemoteModule('price', './Component').then((m) => m.AppComponent),
+      },
+      {
+         path:'home',
+         component:HomeComponent
+      },
+  
+    {
+      path: '**',
+      component: NotfoundComponent,
+    },
+  
+    // DO NOT insert routes after this one.
+    // { path:'**', ...} needs to be the LAST one.
+  ];
